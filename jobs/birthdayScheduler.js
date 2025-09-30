175e25b03bfe0954d8bba30874f937dcc3c715a7
Abasiakara <abasiakaraalexander@gmail.com>
2025-09-30 15:35:43 +0100

    Using sendgrid to send emails

--- jobs/birthdayScheduler.js
+++ jobs/birthdayScheduler.js
@@ -3,25 +3,50 @@ const nodemailer = require("nodemailer");
 const User = require("../models/user");
 const moment = require("moment-timezone");
 require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// const sendBirthdayEmail = async (user) => {
+//   await transporter.sendMail({
+//     from: `"Birthday App" <${process.env.EMAIL_USER}>`,
+//     to: user.email,
+//     subject: "Happy Birthday 🎉",
+//     text: `Hi ${user.username}, happy birthday! Wishing you the best year ahead!`,
+//   });
+// };
 
 const sendBirthdayEmail = async (user) => {
-  await transporter.sendMail({
-    from: `"Birthday App" <${process.env.EMAIL_USER}>`,
+  const msg = {
     to: user.email,
+    from: {
+      email: process.env.EMAIL_USER,
+      name: "Birthday App",
+    },
     subject: "Happy Birthday 🎉",
     text: `Hi ${user.username}, happy birthday! Wishing you the best year ahead!`,
-  });
+    html: `<p>Hi <strong>${user.username}</strong>, happy birthday! 🎂🎉<br/><br/>Wishing you the best year ahead!</p>`,
+  };
+
+  try {
+    await sgMail.send(msg);
+    // console.log("Birthday email sent to:", user.email);
+  } catch (error) {
+    console.error("Error sending birthday email:", error);
+    if (error.response) {
+      console.error(error.response.body);
+    }
+  }
 };
 
-cron.schedule("0 7 * * *", async () => {
+cron.schedule("* * * * *", async () => {
   try {
     const users = await User.find({});
 
