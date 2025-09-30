93e6148c0f54cc61aa6cf9f19c7fa0c6b7d125c3
Abasiakara <abasiakaraalexander@gmail.com>
2025-09-30 20:09:59 +0100

    Using sendgrid to send emails

--- jobs/birthdayScheduler.js
+++ jobs/birthdayScheduler.js
@@ -1,27 +1,9 @@
 const cron = require("node-cron");
-const nodemailer = require("nodemailer");
 const User = require("../models/user");
 const moment = require("moment-timezone");
 require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
-
-// const sendBirthdayEmail = async (user) => {
-//   await transporter.sendMail({
-//     from: `"Birthday App" <${process.env.EMAIL_USER}>`,
-//     to: user.email,
-//     subject: "Happy Birthday 🎉",
-//     text: `Hi ${user.username}, happy birthday! Wishing you the best year ahead!`,
-//   });
-// };
 
 const sendBirthdayEmail = async (user) => {
   const msg = {
@@ -32,12 +14,11 @@ const sendBirthdayEmail = async (user) => {
     },
     subject: "Happy Birthday 🎉",
     text: `Hi ${user.username}, happy birthday! Wishing you the best year ahead!`,
-    html: `<p>Hi <strong>${user.username}</strong>, happy birthday! 🎂🎉<br/><br/>Wishing you the best year ahead!</p>`,
+    html: `<p>Hi <strong>${user.username}</strong>, happy birthday! 🎂🎉<br/>Wishing you the best year ahead!</p>`,
   };
 
   try {
     await sgMail.send(msg);
-    // console.log("Birthday email sent to:", user.email);
   } catch (error) {
     console.error("Error sending birthday email:", error);
     if (error.response) {
@@ -46,26 +27,29 @@ const sendBirthdayEmail = async (user) => {
   }
 };
 
-cron.schedule("* * * * *", async () => {
+cron.schedule("0 7 * * *", async () => {
   try {
     const users = await User.find({});
 
     for (const user of users) {
       const localToday = moment().tz(user.timezone).format("MM-DD");
-      const userBirthday = moment(user.dob).format("MM-DD");
+      const userBirthday = moment(user.dob).tz(user.timezone).format("MM-DD");
 
       if (localToday === userBirthday) {
-        const lastYearSent = user.lastBirthdayEmailSent
+        const lastYearSent = user?.lastBirthdayEmailSent
           ? moment(user.lastBirthdayEmailSent).tz(user.timezone).year()
           : null;
         const currentYear = moment().tz(user.timezone).year();
 
         if (lastYearSent !== currentYear) {
           await sendBirthdayEmail(user);
-
-          await User.findByIdAndUpdate(user._id, {
-            lastBirthdayEmailSent: new Date(),
-          });
+          try {
+            await User.findByIdAndUpdate(user._id, {
+              lastBirthdayEmailSent: new Date(),
+            });
+          } catch (err) {
+            console.log(err);
+          }
         }
       }
     }
